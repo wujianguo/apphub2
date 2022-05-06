52f717ead8f06e11f5d60985fe05917155fd9a20
wujianguo <lsjustin89@gmail.com>
2022-05-06 13:17:40 +0800

    feat: document and more tests

---
+++ util/exception.py
@@ -0,0 +1,8 @@
+from rest_framework.views import exception_handler
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    if response is not None:
+        response.data['code'] = response.status_code
+        response.data['message'] = ''.join(response.data.get('non_field_errors', []))
+    return response
